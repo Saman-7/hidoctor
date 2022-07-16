686097dfa01d3b46ced6d8e5245cfec036e3fc44
saman <saman.explosion.021@gmail.com>
2022-07-16 13:29:40 +0430

    Create list of contents in main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/Header";
+import Main from "./component/Main";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 };
